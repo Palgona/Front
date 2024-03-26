39c5068a5733b5cde6a414e59b19dea2e30ac117
ryong1 <emily1965@naver.com>
2024-03-26 15:59:35 +0900

    design: 커스텀drawer

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import Home from "../Page/Home";
-import { colors, theme } from '../styles/theme';
-import { View, StyleSheet } from 'react-native';
+import { createDrawerNavigator, DrawerItemList } from "@react-navigation/drawer";
+import { colors } from '../styles/theme';
+import { View, StyleSheet, Text } from 'react-native';
 import { StackNavigator } from "./StackNavigator";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Drawer = createDrawerNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.white, // 배경색 설정 가능
   },
   drawerContent: {
     flex: 1,
-    paddingTop: 50,
+    paddingTop: '20%',
+    backgroundColor: 'white',
   },
 });
 
@@ -23,27 +23,34 @@ const drawerStyles = {
     drawerActiveBackgroundColor: colors.secondYellow,
     drawerActiveTintColor: colors.darkGray,
     drawerPosition: 'right',
-    paddingTop: 50,
   },
 };
+
+const CustomDrawerContent = (props) => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.drawerContent}>
+      {/* 네비게이션 메뉴 명시적으로 추가 */}
+      <DrawerItemList {...props} />
+    </View>
+  </SafeAreaView>
+);
 
 const Drawernavigator = () => {
     return (
         <View style={styles.container}>
             <Drawer.Navigator
                 initialRouteName="Home"
-                drawerPosition="right"
-                paddingTop="50"
+                backBehavior="history"
                 screenOptions={{
                     ...drawerStyles.screenOptions,
-                    contentContainerStyle: styles.drawerContent,
-                }}>
+                }}
+                drawerContent={props => <CustomDrawerContent {...props} />}>
                 <Drawer.Screen name="홈" component={StackNavigator} options={{headerShown: false}}/>
-                <Drawer.Screen name="디지털기기" component={Home} options={{ headerShown: false }}/>
-                <Drawer.Screen name="의류" component={Home} options={{headerShown: false}}/>
-                <Drawer.Screen name="식품" component={Home} options={{ headerShown: false }}/>
-                <Drawer.Screen name="도서" component={Home} options={{headerShown: false}}/>
-                <Drawer.Screen name="기타" component={Home} options={{ headerShown: false}}/>
+                <Drawer.Screen name="디지털기기" component={StackNavigator} options={{ headerShown: false }}/>
+                <Drawer.Screen name="의류" component={StackNavigator} options={{headerShown: false}}/>
+                <Drawer.Screen name="식품" component={StackNavigator} options={{ headerShown: false }}/>
+                <Drawer.Screen name="도서" component={StackNavigator} options={{headerShown: false}}/>
+                <Drawer.Screen name="기타" component={StackNavigator} options={{ headerShown: false}}/>
             </Drawer.Navigator>
         </View>
     );
