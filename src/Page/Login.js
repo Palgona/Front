04a6f3a2c4bc4fb568787fdc380bb4d3a2f36d7c
hoosa <janghoosa@gmail.com>
2024-04-04 19:08:03 +0900

    feat: 토큰 받아오는 것까지 구현 (#20)

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,90 +1,58 @@
-import {Pressable, StyleSheet, View, Image} from 'react-native';
-import React, {useState} from 'react';
-import {
-  login,
-  getProfile as getKakaoProfile,
-} from '@react-native-seoul/kakao-login';
+import {Image, Pressable, StyleSheet, View} from 'react-native';
+import React from 'react';
+import {login} from '@react-native-seoul/kakao-login';
 import {API_URL} from '../globalVariables.js';
-import {storeAccessToken, getAccessToken, removeAccessToken} from '../token.js';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = ({navigation}) => {
-  const [result, setResult] = useState('');
-
   const signInWithKakao = async () => {
     try {
-      const token = await login();
-
+      const kakaoOAthtoken = await login();
+      console.log('kakaoOAthtoken', kakaoOAthtoken);
       // 클라이언트에서 토큰을 서버로 전달
-      await sendTokenToBackend(token);
+      await sendTokenToBackend(kakaoOAthtoken);
 
       // 사용자 정보 가져오기
-      const userProfile = await getKakaoProfile();
-      console.log('User Profile:', userProfile);
-
-      setResult(JSON.stringify(token)); // 토큰을 문자열로 표시하지 않음
-      //console.log("Token:", token);
+      // const userProfile = await getKakaoProfile();
+      // console.log('User Profile:', userProfile);
 
       // 로그인 성공 시 Signup 페이지로 이동
       navigation.navigate('Signup');
     } catch (err) {
-      console.error('login err', err);
+      console.error('signInWithKakao err', err);
     }
   };
 
-  const sendTokenToBackend = async token => {
+  const sendTokenToBackend = async kakaoOAthtoken => {
     try {
-      // 사용자 정보 토큰을 가져와서 accessToken 헤더에 설정
-      const userProfile = await getKakaoProfile();
-      const {nickName, profileImage} = userProfile;
-      const accessToken = token.accessToken;
-
       // 액세스 토큰이 만료되었는지 확인
-      if (isAccessTokenExpired(token)) {
-        // 만료되었으면 리프레시 토큰을 사용하여 새로운 액세스 토큰 발급
-        const newAccessToken = await refreshAccessToken(token.refreshToken);
-        // 새로 발급받은 액세스 토큰으로 대체
-        token.accessToken = newAccessToken;
-      }
+      // if (isAccessTokenExpired(token)) {
+      //   // 만료되었으면 리프레시 토큰을 사용하여 새로운 액세스 토큰 발급
+      //   const newAccessToken = await refreshAccessToken(token.refreshToken);
+      //   // 새로 발급받은 액세스 토큰으로 대체
+      //   token.accessToken = newAccessToken;
+      // }
 
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + accessToken,
-      };
-      const body = JSON.stringify({
-        nickName,
-        profileImage,
-      });
-
-      // auth/refresh-token에 대한 요청
-      /*
-      const responseRefresh = await fetch(API_URL+'/auth/refresh-token', {
-        method: 'POST',
-        headers: headers
-      });
-
-      if (!responseRefresh.ok) {
-        throw new Error('Failed to refresh token');
-      }
-
-      console.log('Token refreshed successfully');
-*/
-      // auth/login에 대한 요청
-      const responseLogin = await fetch(API_URL + '/auth/login', {
-        method: 'GET',
-        headers: headers,
-      });
-
-      if (!responseLogin.ok) {
-        throw new Error('로그인 실패!');
-      }
+      const responseLogin = await axios
+        .get(API_URL + '/auth/login', {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'BEARER ' + kakaoOAthtoken.accessToken,
+          },
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log('response', error.response.data);
+          } else if (error.request) {
+            console.log('request', error.request);
+          }
+        });
 
       // 서버로부터 받은 응답 헤더에서 Authorization 값을 가져옴
       const authorizationHeader = responseLogin.headers.get('Authorization');
-      console.log('Authorization Header:', authorizationHeader);
-      await storeAccessToken(authorizationHeader);
-
-      const data = await responseLogin.json();
-      console.log('User logged in successfully:', data);
+      await AsyncStorage.setItem('token', authorizationHeader);
+      console.log('User logged in successfully:', authorizationHeader);
     } catch (err) {
       console.error('Error signing in:', err);
     }
